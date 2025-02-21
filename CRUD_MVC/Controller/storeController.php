ad94605711ede30d8b9ca204dfa53f5a1cb395c2
lbenpen2305 <126820199+lbenpen2305@users.noreply.github.com>
2025-02-21 17:52:26 +0100

    Update storeController.php

--- CRUD_MVC/Controller/storeController.php
+++ CRUD_MVC/Controller/storeController.php
@@ -1,7 +1,62 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+require_once '../Model/store.php';
 
+class storeController {
+
+    public $page_title;
+    public $view;
+    private $noteObj;
+
+    // Construct
+    public function __construct(){
+        $this->view = 'list_store';
+        $this->page_title= '';
+        $this->noteObj = new Store();
+
+    }
+    
+    // List all stores
+    public function list() {
+        $this->page_title = 'Store list';
+        return $this->storeObj->getStores();
+    }
+
+
+    // Load stores for edit
+
+    public function edit($cod = null) {
+        $this->page_title = 'Edit store';
+        $this->view = 'edit_store';
+
+        /*cod can from get param or method param */
+        if(isset($_GET["cod"])) $cod = $_GET["cod"];
+        return $this->storeObj->getStoreByCod($cod);
+
+    }
+
+    // Create or update stores
+    public function save() {
+        $this->view = 'edit_store';
+        $this->page_title = 'Edit store';
+        $cod = $this->storeObj->save($_POST);
+        $result = $this->storeObj->getStoreByCod($cod);
+        $_GET["response"] = true;
+        return $result;
+    }
+
+    // Confirm delete
+    public function confirmDelete() {
+        $this->page_title = 'Delete store';
+        $this->view = 'confirm_delete_store';
+        return $this->storeObj->getStoreByCod($_GET["cod"]);
+    }
+
+    // Delete
+    public function delete() {
+        $this->page_title = 'Store list';
+        $this->view = 'delete_store';
+        return $this->storeObj->deleteStoreByCod($_POST["cod"]);
+    }
+
+}
