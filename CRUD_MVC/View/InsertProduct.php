43d40ef3abfacda195cd49a7ceea02c7d0d4d39f
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-24 19:54:05 +0100

    Add files via upload

--- CRUD_MVC/View/InsertProduct.php
+++ CRUD_MVC/View/InsertProduct.php
@@ -1,11 +1,20 @@
-<form id="insert" action="insertProduct" method="post">
+<form id="insert" action="?accion=insertProduct" method="post">
     <table>
         <tr>
             <td><input type="text" placeholder="cod" name="insert_cod" required></td>
             <td><input type="text" placeholder="short name" name="insert_name" required></td>
             <td><textarea name="insert_description" placeholder="description" rows="5" cols="30" required></textarea></td>
             <td><input type="text" name="insert_RRP" placeholder="RRP" required></td>
-            <td><input type="text" name="insert_family" placeholder="Family" required></td>
+            <td>
+                <select name="insert_family">
+                    <?php
+                    foreach ($families as $fam) {
+                        echo "<option value='${fam['cod']}'>${fam['name']}</option>";
+                    }
+                    echo "</select></td>";
+                    ?>
+                </select>
+            </td>
             <td><input type="submit" value="Insert" name="insert"></td>
         </tr>
     </table>
--- CRUD_MVC/View/listProducts.php
+++ CRUD_MVC/View/listProducts.php
@@ -1,25 +1,30 @@
 <?php
-
 foreach ($products as $product) {
-    echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>";
-    echo '<tr>';
-    echo "<td><input type='text' name='cod' value=' ${product['cod']}'></td>";
-    echo "<td><input type='text' name='short_name' value=' ${product['short_name']}'</td>";
-    echo "<td><textarea name='description' name products='5' cols='30'>${product['description']}</textarea></td>";
-    echo "<td><input type='text' name='rrp' value=' ${product['RRP']}'></td>";
-    echo "<td><select name='family'>";
-    foreach ($data as $dat) {
-        if ($dat['cod'] == $product['family']) {
-            echo "<option value='${dat['cod']}' selected>${dat['name']}</option>";
-        } else {
-            echo "<option value='${dat['cod']}'>${dat['name']}</option>";
-        }
-    }
-    echo "</select></td>";
-    echo '<td><input type="submit" value="Update" name="update"</td>';
-    echo '<td><input type="submit" value="Delete" name="delete"</td>';
-    echo "<input type=hidden name='delete_code' value='${product['cod']}'>";
-    echo '</tr>';
-    echo '</form>';
+    ?><form action="index.php" method='post' id="updateDelete">
+        <tr>
+            <td><input type='text' name='cod' value='<?= $product['cod'] ?>'></td>
+            <td><input type='text' name='short_name' value='<?= $product['short_name'] ?>'</td>
+            <td><textarea name='description' rows='5' cols='30'><?= $product['description'] ?></textarea></td>
+            <td><input type='text' name='rrp' value='<?= $product['RRP'] ?>'></td>
+            <td><select name='family'>
+                    <?php
+                    foreach ($families as $fam) {
+                        if ($fam['cod'] == $product['family']) {
+                            ?>
+                            <option selected value="<?php $fam['cod'] ?>"><?= $fam['name'] ?></option>
+                            <?php } else {
+                            ?>
+                            <option value="<?= $fam['cod'] ?>"><?= $fam['name'] ?></option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select></td>
+            <td><button type="submit" formaction="index.php?accion=updateProduct">Update</button></td>
+            <td><button type="submit" formaction= 'index.php?accion=deleteProduct'>Delete</button></td>
+        <input type=hidden name='delete_code' value='<?= $product['cod'] ?>'>
+        </tr>
+    </form>
+    <?php
 }
 ?>
