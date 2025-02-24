3f6d947d2618f2f7a5c722200715da87ac5f16d3
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-24 19:53:15 +0100

    Update product.php

--- CRUD_MVC/Model/product.php
+++ CRUD_MVC/Model/product.php
@@ -1,5 +1,36 @@
 <?php
 
-class Product{
-    
+require_once 'db.php';
+
+class Product {
+
+    public static function getProducts() {
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = "SELECT cod, short_name, description, RRP, family FROM product";
+        $result = $dwes->query($sql);
+        $products = $result->fetchAll();
+        unset($dwes);
+        return $products;
+    }
+
+    public static function updateProduct($prevCode, $cod, $shortName, $rrp, $desc, $family) {
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('UPDATE product SET short_name=?, cod=?, description=?, RRP=?, family=?  WHERE product.cod=?');
+        $sql->execute([$shortName, $cod, $desc, $rrp, $family, $prevCode]);
+        unset($dwes);
+    }
+
+    public static function deleteProduct($cod) {
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('DELETE FROM product WHERE product.cod = ?');
+        $sql->execute([$cod]);
+        unset($dwes);
+    }
+
+    public static function insertProduct($cod, $shortName, $rrp, $desc, $family) {
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('INSERT INTO product (cod, short_name, description, RRP, family) VALUES (?, ?, ?, ?, ?);');
+        $sql->execute([$cod, $shortName, $desc, $rrp, $family]);
+        unset($dwes);
+    }
 }
