f16e7fc3f2de3467feb4ced224b19351290f7b40
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-24 20:08:18 +0100

    Update CRUDController.php

--- CRUD_MVC/Controller/CRUDController.php
+++ CRUD_MVC/Controller/CRUDController.php
@@ -23,26 +23,32 @@ class Controllers {
     }
 
     public static function insertProduct() {
-        $shortName = trim($_POST['insert_name']);
-        $rrp = trim($_POST['insert_RRP']);
-        $cod = trim($_POST['insert_cod']);
-        $desc = trim($_POST['insert_description']);
-        $family = trim($_POST['insert_family']);
-        \Product::insertProduct($cod, $shortName, $rrp, $desc, $family);
+        if (isset($_POST['insert_name'])) {
+            $shortName = trim($_POST['insert_name']);
+            $rrp = trim($_POST['insert_RRP']);
+            $cod = trim($_POST['insert_cod']);
+            $desc = trim($_POST['insert_description']);
+            $family = trim($_POST['insert_family']);
+            \Product::insertProduct($cod, $shortName, $rrp, $desc, $family);
+        }
     }
 
     public static function deleteProduct() {
-        $prevCode = trim($_POST['delete_code']);
-        \Product::deleteProduct($prevCode);
+        if (isset($_POST['delete_code'])) {
+            $prevCode = trim($_POST['delete_code']);
+            \Product::deleteProduct($prevCode);
+        }
     }
 
     public static function updateProduct() {
-        $prevCode = trim($_POST['delete_code']);
-        $shortName = trim($_POST['short_name']);
-        $rrp = trim($_POST['rrp']);
-        $cod = trim($_POST['cod']);
-        $desc = trim($_POST['description']);
-        $family = trim($_POST['family']);
-        \Product::updateProduct($prevCode, $cod, $shortName, $rrp, $desc, $family);
+        if (isset($_POST['delete_code'])) {
+            $prevCode = trim($_POST['delete_code']);
+            $shortName = trim($_POST['short_name']);
+            $rrp = trim($_POST['rrp']);
+            $cod = trim($_POST['cod']);
+            $desc = trim($_POST['description']);
+            $family = trim($_POST['family']);
+            \Product::updateProduct($prevCode, $cod, $shortName, $rrp, $desc, $family);
+        }
     }
 }
