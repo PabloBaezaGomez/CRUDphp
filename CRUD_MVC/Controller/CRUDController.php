7044386221360fc9f7666661b2b1a2eef408bd32
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-24 19:52:30 +0100

    Add files via upload

--- CRUD_MVC/Controller/CRUDController.php
+++ CRUD_MVC/Controller/CRUDController.php
@@ -13,15 +13,36 @@ class Controllers {
         require_once 'View/buttonProduct.php';
         require_once 'View/buttonStock.php';
         require_once 'View/buttonStore.php';
-        
-        
-        
     }
-    
+
     public static function CRUDControllerProducts() {
+        $families = \Family::getFamilies();
         require_once 'View/InsertProduct.php';
         $products = \Product::getProducts();
         require_once 'View/listProducts.php';
-        
+    }
+
+    public static function insertProduct() {
+        $shortName = trim($_POST['insert_name']);
+        $rrp = trim($_POST['insert_RRP']);
+        $cod = trim($_POST['insert_cod']);
+        $desc = trim($_POST['insert_description']);
+        $family = trim($_POST['insert_family']);
+        \Product::insertProduct($cod, $shortName, $rrp, $desc, $family);
+    }
+
+    public static function deleteProduct() {
+        $prevCode = trim($_POST['delete_code']);
+        \Product::deleteProduct($prevCode);
+    }
+
+    public static function updateProduct() {
+        $prevCode = trim($_POST['delete_code']);
+        $shortName = trim($_POST['short_name']);
+        $rrp = trim($_POST['rrp']);
+        $cod = trim($_POST['cod']);
+        $desc = trim($_POST['description']);
+        $family = trim($_POST['family']);
+        \Product::updateProduct($prevCode, $cod, $shortName, $rrp, $desc, $family);
     }
 }
