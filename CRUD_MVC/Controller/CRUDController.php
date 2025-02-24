deb84f75569e867fa9f22912ca50768b0d7d28cd
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-24 18:17:42 +0100

    Add files via upload

--- CRUD_MVC/Controller/CRUDController.php
+++ CRUD_MVC/Controller/CRUDController.php
@@ -2,5 +2,26 @@
 
 namespace CRUD\controllers;
 
-use CRUD\exceptions\AppException;
-use CRUD\model\Family;
+require_once 'Model/product.php';
+require_once 'Model/family.php';
+
+class Controllers {
+
+    public static function default() {
+        // show buttons
+        require_once 'View/buttonFamily.php';
+        require_once 'View/buttonProduct.php';
+        require_once 'View/buttonStock.php';
+        require_once 'View/buttonStore.php';
+        
+        
+        
+    }
+    
+    public static function CRUDControllerProducts() {
+        require_once 'View/InsertProduct.php';
+        $products = \Product::getProducts();
+        require_once 'View/listProducts.php';
+        
+    }
+}
