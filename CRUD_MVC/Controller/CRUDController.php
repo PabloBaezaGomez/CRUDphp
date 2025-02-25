b196394e61d316fe23b1c2993c1f4f6f29f0f444
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 20:15:13 +0100

    Update CRUDController.php

--- CRUD_MVC/Controller/CRUDController.php
+++ CRUD_MVC/Controller/CRUDController.php
@@ -87,6 +87,38 @@ class Controllers {
     }
     
     //Table Stock
-    
+    public static function CRUDControllerStock() {
+        $stocks = \Stock::getStocks();
+        $products = \Product::getProductsDB();
+        $stores = \Store::getStores();
+        require_once 'View/InsertStock.php';
+        require_once 'View/listStock.php';
+    }
+
+    public static function insertStock() {
+        if (isset($_POST['insert_units'])) {
+            $product = trim($_POST['insert_product']);
+            $store = trim($_POST['insert_store']);
+            $units = trim($_POST['insert_units']);
+            \Stock::saveStock($product, $store, $units);
+        }
+    }
+
+    public static function deleteStock() {
+        if (isset($_POST['product'])) {
+            $product = trim($_POST['product']);
+            $store = trim($_POST['store']);
+            \Stock::deleteStock($product, $store);
+        }
+    }
+
+    public static function updateStock() {
+        if (isset($_POST['product'])) {
+            $product = trim($_POST['product']);
+            $store = trim($_POST['store']);
+            $units = trim($_POST['units']);
+            \Stock::saveStock($product, $store, $units);
+        }
+    }
     //Table Stores
 }
