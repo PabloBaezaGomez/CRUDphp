2cb5837522a26ab292a754a76879a479bc7dbf85
lbenpen2305 <126820199+lbenpen2305@users.noreply.github.com>
2025-02-25 19:54:18 +0100

    Update InsertStore.php

--- CRUD_MVC/View/InsertStore.php
+++ CRUD_MVC/View/InsertStore.php
@@ -5,6 +5,7 @@
             <td><input type="text" placeholder="name" name="insert_name" required></td>
             <td><input type="number" placeholder="tlf" name="insert_tlf" required></td>
 
+
             <td><input type="submit" value="Insert" name="insert"></td>
         </tr>
     </table>
