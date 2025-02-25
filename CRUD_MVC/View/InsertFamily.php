4fba1db07078f6be8aa67d45d5d284d4d556aa22
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 19:50:36 +0100

    Update InsertFamily.php

--- CRUD_MVC/View/InsertFamily.php
+++ CRUD_MVC/View/InsertFamily.php
@@ -1,9 +1,9 @@
-<form  id="insert" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <table>
-                    <tr>
-                        <td><input type="text" placeholder="cod" name="insert_cod" required></td>
-                        <td><input type="text" placeholder="short name" name="insert_name" required></td>
-                        <td><input type="submit" value="Insert" name="insert"></td>
-                    </tr>
-                </table>
-            </form>
+<form  id="insert" action="?accion=insertFamily" method="post">
+    <table>
+        <tr>
+            <td><input type="text" placeholder="cod" name="insert_cod" required></td>
+            <td><input type="text" placeholder="short name" name="insert_name" required></td>
+            <td><input type="submit" value="Insert" name="insert"></td>
+        </tr>
+    </table>
+</form>
