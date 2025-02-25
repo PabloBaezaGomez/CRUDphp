0adf48479c5aba89dde891190c3504e69aff22d0
lbenpen2305 <126820199+lbenpen2305@users.noreply.github.com>
2025-02-25 18:53:40 +0100

    Update store.php

--- CRUD_MVC/Model/store.php
+++ CRUD_MVC/Model/store.php
@@ -1,106 +1,43 @@
 <?php
 
-namespace CRUD\model;
-use CRUD\Db;
-
+require_once 'db.php';
 
 class Store
 {
 
-    private $cod;
-    private $name;
 
-    private $tlf;
-
-    // Construct
-    public function __construct($cod, $name, $tlf)
-    {
-        $this->cod = $cod;
-        $this->name = $name;
-        $this->tlf = $tlf;
-    }
-
-    public function getCod()
-    {
-        return $this->cod;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getTlf()
-    {
-        return $this->tlf;
-    }
-
-    // Select all the data, returns all the data in the table
     public static function getStores()
     {
-        $sql = "SELECT * FROM store";
-        $stmt = db::doSQL($sql);
-        $stmt->execute();
-
-        return $stmt->fetchAll();
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = "SELECT cod, name, tlf FROM store";
+        $result = $dwes->query($sql);
+        $stores = $result->fetchAll();
+        unset($dwes);
+        return $stores;
     }
 
-    // Selects the data by the store cod
-    public function getStoreByCod($cod)
+    public static function updateStores($prevCode, $cod, $name, $tlf)
     {
-        if (is_null($cod))
-            return false;
-
-        $sql = "SELECT * FROM " . $this->table . " WHERE cod = ?";
-        $stmt = db::doSQL($sql, [$cod]);
-
-        return $stmt->fetch();
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('UPDATE store SET cod=?, name=?, tlf=? WHERE store.cod=?');
+        $sql->execute([$name, $cod, $tlf, $prevCode]);
+        unset($dwes);
     }
 
-    public function saveStore($store)
+    public static function deleteStores($cod)
     {
-        $cod = $name = $tlf = "";
-
-        $exists = false;
-
-        if (isset($store["cod"]) and $store["cod"] != '') {
-            $actualStore = $this->getStoreByCod($store["cod"]);
-            if (isset($actualStore["cod"])) {
-                $exists = true;
-                $prevCod = $actualStore["cod"];
-            }
-        }
-
-        // Received values
-        if (isset($store["name"]))
-            $name = $store["name"];
-        if (isset($store["cod"]))
-            $cod = $store["cod"];
-        if (isset($store["tlf"]))
-            $tlf = $store["tlf"];
-
-        if ($cod == "" || $name == "" || $tlf == "") {
-            return;
-        }
-
-        // Database operations if the family already exists, update the values
-        if ($exists) {
-            $sql = "UPDATE " . $this->table . "SET name=?, cod=?, tlf=? WHERE cod=?";
-            $stmt = db::doSQL($sql, [$name, $cod, $prevCod]);
-
-        } else {
-            // If it doesn't, it creates a new one
-            $sql = "INSERT INTO".$this->table."(cod,name,tlf) values(?, ?, ?)";
-            $stmt = db::doSQL($sql, [$name, $cod, $tlf]);
-        }
-
-        return $cod;
-
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('DELETE FROM stores WHERE store.cod = ?');
+        $sql->execute([$cod]);
+        unset($dwes);
     }
 
-    // Deletes a store which key Value is the same as the one of the parametres
-    public function deleteStoreByCod($cod) {
-        $sql = "DELETE FROM " . $this->table . " WHERE cod = ?";
-        return db::doSQL($sql, [$cod]);
+    public static function insertStores($cod, $name, $tlf)
+    {
+        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
+        $sql = $dwes->prepare('INSERT INTO store (cod, name, tlf) VALUES (?, ?, ?);');
+        $sql->execute([$cod, $name, $tlf]);
+        unset($dwes);
     }
 }
+
