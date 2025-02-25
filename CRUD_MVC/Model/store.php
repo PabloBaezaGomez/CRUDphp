96f8a043d9fe9c488d0763861a59445a0527773f
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 19:31:17 +0100

    Update store.php

--- CRUD_MVC/Model/store.php
+++ CRUD_MVC/Model/store.php
@@ -5,39 +5,30 @@ require_once 'db.php';
 class Store
 {
 
-
     public static function getStores()
     {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
         $sql = "SELECT cod, name, tlf FROM store";
-        $result = $dwes->query($sql);
-        $stores = $result->fetchAll();
-        unset($dwes);
+        $result = CRUD\DB::doSQL($sql);
         return $stores;
     }
 
     public static function updateStores($prevCode, $cod, $name, $tlf)
     {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        $sql = $dwes->prepare('UPDATE store SET cod=?, name=?, tlf=? WHERE store.cod=?');
-        $sql->execute([$name, $cod, $tlf, $prevCode]);
-        unset($dwes);
+        $sql = 'UPDATE store SET cod=:cod, name=:name, tlf=:tlf WHERE store.cod=:prevCode';
+        $result = CRUD\DB::doSQL($sql, ['cod'=>$cod,'name'=>$name,'tlf'=>$tlf,'prevCode'=>$prevCode]);
+        return $result;
     }
 
     public static function deleteStores($cod)
     {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        $sql = $dwes->prepare('DELETE FROM stores WHERE store.cod = ?');
-        $sql->execute([$cod]);
-        unset($dwes);
+        $sql = 'DELETE FROM stores WHERE store.cod = ?';
+        $result = CRUD\DB::doSQL($sql, ['cod'=>$cod]);
+        return $result;
     }
 
     public static function insertStores($cod, $name, $tlf)
     {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        $sql = $dwes->prepare('INSERT INTO store (cod, name, tlf) VALUES (?, ?, ?);');
-        $sql->execute([$cod, $name, $tlf]);
-        unset($dwes);
+        $sql = 'INSERT INTO store (cod, name, tlf) VALUES (:cod, :name, :tlf);';
+        $result = CRUD\DB::doSQL($sql, ['cod'=>$cod,'name'=>$name,'tlf'=>$tlf]);
     }
 }
-
