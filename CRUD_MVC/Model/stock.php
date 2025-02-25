9ae37ad40fbe989f3ad504db2f9c4e7cf543d082
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 19:39:38 +0100

    Update stock.php

--- CRUD_MVC/Model/stock.php
+++ CRUD_MVC/Model/stock.php
@@ -5,51 +5,39 @@ require_once 'db.php';
 class Stock {
 
     public static function getStocks() {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
         $sql = "SELECT * FROM stock";
-        $result = $dwes->query($sql);
-        $stocks = $result->fetchAll();
-        unset($dwes);
+        $stocks = CRUD\DB::doSQL($sql);
         return $stocks;
     }
 
     public static function getStockByProduct($product) {
-        if (is_null($product)) return false;
-        
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        $sql = $dwes->prepare("SELECT * FROM stock WHERE product = ?");
-        $sql->execute([$product]);
-        $result = $sql->fetch();
-        unset($dwes);
+        if (is_null($product)){
+            return false;
+        }
+        $sql = ("SELECT * FROM stock WHERE product = :product");
+        $result = CRUD\DB::doSQL($sql, ['product'=>$product]);
         return $result;
     }
 
     public static function saveStock($product, $store, $units) {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        
         // Check if stock exists
-        $sql = $dwes->prepare("SELECT * FROM stock WHERE product = ? AND store = ?");
-        $sql->execute([$product, $store]);
-        $exists = $sql->fetch();
-
+        $sql = "SELECT * FROM stock WHERE product = :product AND store = :store";
+        $exists = CRUD\DB::doSQL($sql, ['product'=>$product,'store'=>$store]);
         if ($exists) {
             // Update existing stock
-            $sql = $dwes->prepare("UPDATE stock SET units = ? WHERE product = ? AND store = ?");
-            $sql->execute([$units, $product, $store]);
+            $sql = "UPDATE stock SET units = :units WHERE product = :product AND store = :store";
+            $product = CRUD\DB::doSQL($sql, ['units'=>$units,'product'=>$product,'store'=>$store]);
         } else {
             // Insert new stock
-            $sql = $dwes->prepare("INSERT INTO stock (product, store, units) VALUES (?, ?, ?)");
-            $sql->execute([$product, $store, $units]);
+            $sql = $dwes->prepare("INSERT INTO stock (product, store, units) VALUES (:product, :store, :units)");
+            $product = CRUD\DB::doSQL($sql, ['product'=>$product,'store'=>$store,'units'=>$units]);
         }
-        
-        unset($dwes);
         return $product;
     }
 
     public static function deleteStock($product, $store) {
-        $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "dwes");
-        $sql = $dwes->prepare("DELETE FROM stock WHERE product = ? AND store = ?");
-        $sql->execute([$product, $store]);
-        unset($dwes);
+        $sql = $dwes->prepare("DELETE FROM stock WHERE product = :product AND store = :store");
+        $result = CRUD\DB::doSQL($sql, ['product'=>$product,'store'=>$store]);
+        return $result;
     }
 }
