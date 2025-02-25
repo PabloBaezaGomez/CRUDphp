8c5b1a627e7f46e814466f5ee02d03ad21a802bf
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 18:23:27 +0100

    Update listFamilies.php

--- CRUD_MVC/View/listFamilies.php
+++ CRUD_MVC/View/listFamilies.php
@@ -1,3 +1,20 @@
-<?php
+<table>
+                <?php
+                $sql = "SELECT cod, name FROM family";
+                $result = $dwes->query($sql);
+                $row = $result->fetch();
+                while ($row) {
+                    echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>";
+                    echo '<tr>';
+                    echo "<td><input type='text' name='cod' value=' ${row['cod']}'></td>";
+                    echo "<td><input type='text' name='name' value=' ${row['name']}'</td>";
+                    echo '<td><input type="submit" value="Update" name="update"</td>';
+                    echo '<td><input type="submit" value="Delete" name="delete"</td>';
+                    echo "<input type=hidden name='delete_code' value='${row['cod']}'>";
+                    echo '</tr>';
+                    echo '</form>';
+                    $row = $result->fetch();
+                }
+                ?>
 
-namespace CRUD\view;
+            </table>
