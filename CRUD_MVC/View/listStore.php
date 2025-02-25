5546499b9e272f3e777181f16a01b1420df22d0d
lbenpen2305 <126820199+lbenpen2305@users.noreply.github.com>
2025-02-25 19:43:29 +0100

    Update listStore.php

--- CRUD_MVC/View/listStore.php
+++ CRUD_MVC/View/listStore.php
@@ -1,21 +1,18 @@
-<table>
-                <?php
-                $sql = "SELECT cod, name, tlf FROM store";
-                $result = $dwes->query($sql);
-                $row = $result->fetch();
-                while ($row) {
-                    echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>";
-                    echo '<tr>';
-                    echo "<td><input type='text' name='cod' value=' ${row['cod']}'></td>";
-                    echo "<td><input type='text' name='name' value=' ${row['name']}'</td>";
-                    echo "<td><input type='text' name='number' value=' ${row['tlf']}'</td>";
-                    echo '<td><input type="submit" value="Update" name="update"</td>';
-                    echo '<td><input type="submit" value="Delete" name="delete"</td>';
-                    echo "<input type=hidden name='delete_code' value='${row['cod']}'>";
-                    echo '</tr>';
-                    echo '</form>';
-                    $row = $result->fetch();
-                }
-                ?>
+<?php
+foreach ($stores as $store) {
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <tr>
+            <td><input type='text' name='cod' value='<?= $store['cod'] ?>'></td>
+            <td><input type='text' name='name' value='<?= $store['name'] ?>'></td>
+            <td><input type='number' name='name' value='<?= $store['tlf'] ?>'></td>
 
-            </table>
+            <td><button type="submit" formaction="index.php?accion=updateStore">Update</button></td>
+            <td><button type="submit" formaction='index.php?accion=deleteStore'>Delete</button></td>
+            <input type=hidden name='delete_code' value='<?= $store['cod'] ?>'>
+
+        </tr>
+    </form>
+    <?php
+}
+?>
