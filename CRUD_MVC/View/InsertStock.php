21bb07ef8082eda7656abb2e2d13ef6147ed562e
1calm-user <jorge.eh.rivero@gmail.com>
2025-02-25 19:53:47 +0100

    Update InsertStock.php

--- CRUD_MVC/View/InsertStock.php
+++ CRUD_MVC/View/InsertStock.php
@@ -1,34 +1,32 @@
-<form id="insert" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <table>
-                    <tr>
-                        <td>
-                            <select name="insert_product">
-                                <?php
-                                $sql = "SELECT cod, short_name FROM product";
-                                $result = $dwes->query($sql);
-                                $row = $result->fetch();
-                                while ($row) {
-                                    echo "<option value='${row['cod']}'>${row['short_name']}</option>";
-                                    $row = $result->fetch();
-                                }
-                                ?>
-                            </select>
-                        </td>
-                        <td>
-                            <select name="insert_store">
-                                <?php
-                                $sql = "SELECT cod, name FROM store";
-                                $result = $dwes->query($sql);
-                                $row = $result->fetch();
-                                while ($row) {
-                                    echo "<option value='${row['cod']}'>${row['name']}</option>";
-                                    $row = $result->fetch();
-                                }
-                                ?>
-                            </select>
-                        </td>
-                        <td><input type="number" placeholder="units" name="insert_units" required></td>
-                        <td><input type="submit" value="Insert" name="insert"></td>
-                    </tr>
-                </table>
-            </form>
+<form id="insert" action="?accion=insertStock" method="post">
+    <table>
+        <tr>
+            <td>
+                <select name="insert_product" required>
+                    <option value="">Select Product</option>
+                    <?php
+                    foreach ($products as $product) {
+                        echo "<option value='${product['cod']}'>${product['short_name']}</option>";
+                    }
+                    ?>
+                </select>
+            </td>
+            <td>
+                <select name="insert_store" required>
+                    <option value="">Select Store</option>
+                    <?php
+                    foreach ($stores as $store) {
+                        echo "<option value='${store['cod']}'>${store['name']}</option>";
+                    }
+                    ?>
+                </select>
+            </td>
+            <td>
+                <input type="number" placeholder="Units" name="insert_units" required>
+            </td>
+            <td>
+                <input type="submit" value="Insert" name="insert">
+            </td>
+        </tr>
+    </table>
+</form>
