e03aecae5952c24470a1486a102bd079f5e812d8
PabloBaezaGomez <161077369+PabloBaezaGomez@users.noreply.github.com>
2025-02-25 20:14:52 +0100

    Update index.php

--- CRUD_MVC/index.php
+++ CRUD_MVC/index.php
@@ -9,15 +9,38 @@ CRUD\controllers\Controllers::default();
 switch ($accion) {
     case "moveProduct":
         CRUD\controllers\Controllers::CRUDControllerProducts();
+        break;
     case "deleteProduct":
         CRUD\controllers\Controllers::deleteProduct();
         CRUD\controllers\Controllers::CRUDControllerProducts();
+        break;
     case "updateProduct":
         CRUD\controllers\Controllers::updateProduct();
         CRUD\controllers\Controllers::CRUDControllerProducts();
+        break;
     case "insertProduct":
         CRUD\controllers\Controllers::insertProduct();
         CRUD\controllers\Controllers::CRUDControllerProducts();
+        break;
+    case "moveFamily":
+        CRUD\controllers\Controllers::CRUDControllerFamilies();
+        break;
+    case "deleteFamily":
+        CRUD\controllers\Controllers::deleteFamily();
+        CRUD\controllers\Controllers::CRUDControllerFamilies();
+        break;
+    case "updateFamily":
+        CRUD\controllers\Controllers::updateFamily();
+        CRUD\controllers\Controllers::CRUDControllerFamilies();
+        break;
+    case "insertFamily":
+        CRUD\controllers\Controllers::insertFamily();
+        CRUD\controllers\Controllers::CRUDControllerFamilies();
+        break;
+    case "moveStock":
+        CRUD\controllers\Controllers::CRUDControllerStock();
+        break;
+        case :
 }
 
 
