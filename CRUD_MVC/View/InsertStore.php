2c8607ad9130836cc6af2d7df7e4031c12dc8c10
lbenpen2305 <126820199+lbenpen2305@users.noreply.github.com>
2025-02-25 19:53:37 +0100

    Update InsertStore.php

--- CRUD_MVC/View/InsertStore.php
+++ CRUD_MVC/View/InsertStore.php
@@ -1,10 +1,11 @@
-<form  id="insert" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <table>
-                    <tr>
-                        <td><input type="text" placeholder="cod" name="insert_cod" required></td>
-                        <td><input type="text" placeholder="short name" name="insert_name" required></td>
-                        <td><input type="text" placeholder="Phone Number" name="insert_number" required></td>
-                        <td><input type="submit" value="Insert" name="insert"></td>
-                    </tr>
-                </table>
-            </form>
+<form id="insert" action="?accion=insertStore" method="post">
+    <table>
+        <tr>
+            <td><input type="text" placeholder="cod" name="insert_cod" required></td>
+            <td><input type="text" placeholder="name" name="insert_name" required></td>
+            <td><input type="number" placeholder="tlf" name="insert_tlf" required></td>
+
+            <td><input type="submit" value="Insert" name="insert"></td>
+        </tr>
+    </table>
+</form>
